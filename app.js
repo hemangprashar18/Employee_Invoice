f80a313c5be6fb93bdd0a2edefc81543028155e3
hemangprashar18 <hemangprashar18@github.com>
2024-09-12 18:26:35 +0530

    recent changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://thome3100:MspXD2YvRaOflqfR@test-prod.30wxq.mongodb.net/?retryWrites=true&w=majority&appName=test-prod')
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch(err => console.error('Error connecting to MongoDB', err));
 
